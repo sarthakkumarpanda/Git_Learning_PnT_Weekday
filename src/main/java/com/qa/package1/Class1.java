c9f971f917abb03394f5198af0309b6e6ecf5331
sarthakkumarpanda <sarthak.matrix@gmail.com>
2023-09-20 19:30:07 -0400

    first time code push

---
+++ src/main/java/com/qa/package1/Class1.java
@@ -0,0 +1,10 @@
+package com.qa.package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("The Sky is Blue");
+
+	}
+
+}
---
+++ src/main/java/com/qa/package1/Class2.java
@@ -0,0 +1,10 @@
+package com.qa.package1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("The grass is green");
+
+	}
+
+}
