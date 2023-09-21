a38f75fdae10c1683ae59d663120c747bcc80acb
HemanthChirikonda <67725821+HemanthChirikonda@users.noreply.github.com>
2023-09-20 21:18:51 -0400

    class 5 created

---
+++ src/main/java/com/qa/package1/Class5.java
@@ -0,0 +1,10 @@
+package com.qa.package1;
+
+public class Class5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("this is from Hemanth");
+	}
+
+}
