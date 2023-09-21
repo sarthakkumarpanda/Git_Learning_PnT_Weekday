b8250b2e78c3cc9ea0512aacb126147dbc7234a1
sarthakkumarpanda <sarthak.matrix@gmail.com>
2023-09-20 21:29:44 -0400

    added an addition method

--- src/main/java/com/qa/package1/Class4.java
+++ src/main/java/com/qa/package1/Class4.java
@@ -4,7 +4,13 @@ public class Class4 {
 
 	public static void main(String[] args) {
 		System.out.println("working with selenium");
+		addition();
 
+	}
+	
+	public static void addition() {
+		int i=2, j=3;
+		System.out.println(i+j);
 	}
 
 }
